3f29e6f68d28170c676367cb26944705494c3144
Paul Oberg <pauloberg1931@gmail.com>
2026-05-29 19:37:28 +0100

    Add MatchingEngine usage and sample order in main()
    Included MatchingEngine.hpp and created an instance in main().

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
+#include "MatchingEngine.hpp"
+
 #include <iostream>
 
 int main()
 {
-    std::cout << "Matching Engine\n";
+    MatchingEngine engine;
+
+    Order order{
+        .id = 1,
+        .side = Side::Buy,
+        .type = OrderType::Limit,
+        .price = 100.0,
+        .quantity = 50,
+        .timestamp = 1
+    };
+
+    engine.submitOrder("AAPL", order);
+
+    std::cout << "Matching Engine Started\n";
+
     return 0;
 }
