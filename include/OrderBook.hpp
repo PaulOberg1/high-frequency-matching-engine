1fedb5fffe0385a21cd7b1c1224f2578ecfbd076
Paul Oberg <pauloberg1931@gmail.com>
2026-05-29 20:32:29 +0100

    Add basic support for Market and Limit Buy orders

--- include/OrderBook.hpp
+++ include/OrderBook.hpp
@@ -1,13 +1,17 @@
 #pragma once
 
 #include "Order.hpp"
+#include <map>
+#include <deque>
 
 class OrderBook
 {
 public:
     OrderBook() = default;
 
-    void processOrder(const Order& order);
+    void processOrder(Order order);
 
 private:
+    std::map<double, std::deque<Order>, std::greater<double>> m_buyOrders;
+    std::map<double, std::deque<Order>, std::less<double>> m_sellOrders;
 };
--- src/OrderBook.cpp
+++ src/OrderBook.cpp
@@ -1,6 +1,39 @@
 #include "OrderBook.hpp"
-
-void OrderBook::processOrder(const Order& order)
+void OrderBook::processOrder(Order order)
 {
-    (void)order;
+    if (order.side == Side::Buy)
+    {
+        auto sellIt = m_sellOrders.begin();
+
+        while (order.quantity > 0 &&
+            sellIt != m_sellOrders.end() &&
+            (order.type == OrderType::Market || sellIt->first <= order.price))
+        {
+            auto& orderQueue = sellIt->second;
+
+            while (order.quantity > 0 && !orderQueue.empty())
+            {
+                Order& sellOrder = orderQueue.front();
+
+                uint32_t executedQuantity =
+                    std::min(order.quantity, sellOrder.quantity);
+
+                order.quantity -= executedQuantity;
+                sellOrder.quantity -= executedQuantity;
+
+                if (sellOrder.quantity == 0)
+                    orderQueue.pop_front();
+            }
+
+            if (orderQueue.empty())
+                sellIt = m_sellOrders.erase(sellIt);
+            else
+                break;
+        }
+
+        if (order.type == OrderType::Limit && order.quantity > 0)
+        {
+            m_buyOrders[order.price].push_back(order);
+        }
+    }
 }
